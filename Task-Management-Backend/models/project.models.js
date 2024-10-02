b8827e055bad633bb537598fd5bca9143e7e79f9
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2024-10-02 10:00:50 +0530

    removed duplicates

--- Task-Management-Backend/models/project.models.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const ProjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-});
-
-export default mongoose.model("Project", ProjectSchema);
