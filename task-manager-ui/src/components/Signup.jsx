9d229f347e862c4585c1a2f17bf578dc2b3b9fdc
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2024-10-02 23:31:36 +0530

    SIgn up button ui

--- task-manager-ui/src/components/Signup.jsx
+++ task-manager-ui/src/components/Signup.jsx
@@ -84,7 +84,7 @@ export default function Register() {
               type="submit"
               className="w-full bg-blue-400 hover:bg-blue-600 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
-              Sign in
+              Sign Up
             </button>
             <p className="text-sm font-light text-gray-500">
               Don't have an account yet?{" "}
